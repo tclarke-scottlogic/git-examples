211ce805207bc99a6dec1dab51df45bb5b2b6c55
Thomas Clarke <tclarke@scottlogic.com>
2023-05-22 10:15:02 +0100

    Rename

--- BasicProject/src/main/java/SourceFile.java
+++ BasicProject/src/main/java/SourceFile.java
@@ -1,9 +1,9 @@
 public class SourceFile {
-    public static int getValue() {
+    public static int getVersion() {
         return 1;
     }
 
     public static void main(String[] args){
-        System.out.printf("Version: %d%n", getValue());
+        System.out.printf("Version: %d%n", getVersion());
     }
 }
--- BasicProject/src/test/java/TestFile.java
+++ BasicProject/src/test/java/TestFile.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TestFile {
     @Test
-    void getValue() {
-        assertEquals(1, SourceFile.getValue());
+    void getVersion() {
+        assertEquals(1, SourceFile.getVersion());
     }
 }
