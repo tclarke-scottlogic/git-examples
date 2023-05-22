d67e9e8129c9470e9b33224018d87106ee61660b
Thomas Clarke <tclarke@scottlogic.com>
2023-05-22 10:12:12 +0100

    V1 tests

--- BasicProject/src/test/java/TestFile.java
+++ BasicProject/src/test/java/TestFile.java
@@ -1,4 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TestFile {
-  
+    @Test
+    void getValue() {
+        assertEquals(1, SourceFile.getValue());
+    }
 }
